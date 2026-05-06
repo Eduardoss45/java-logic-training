84a607f6c459cd15c6a918d3619b5cadfeb56c8c
Eduardoss45 <temporariac432@gmail.com>
2026-05-06 19:20:14 -0300

    Exercício 22.

---
+++ src/main/java/br/com/eduardo/logic/condicionais/Ex22ModuloDeUmNumero.java
@@ -0,0 +1,25 @@
+package br.com.eduardo.logic.condicionais;
+
+// * 22. Escreva um programa que leia um número e exiba o seu módulo.
+
+import java.util.Scanner;
+
+public class Ex22ModuloDeUmNumero {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite um número: ");
+            int numero = scanner.nextInt();
+            int res;
+            if (numero > 0) {
+                res = numero;
+                System.out.printf("O modulo de %d é %d", numero, res);
+            } else if (numero < 0) {
+                res = numero * -1;
+                System.out.printf("O modulo de %d é %d", numero, res);
+            } else {
+                res = 0;
+                System.out.printf("O módulo de %d é %d%n", numero, res);
+            }
+        }
+    }
+}
