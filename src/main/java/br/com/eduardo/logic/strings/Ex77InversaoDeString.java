c7dd9d60af0eba84d8c2c73fd1bd40ccb140c477
Eduardoss45 <temporariac432@gmail.com>
2026-08-05 17:58:15 -0300

    feat: Exercício 77.

---
+++ src/main/java/br/com/eduardo/logic/strings/Ex77InversaoDeString.java
@@ -0,0 +1,26 @@
+// * 77. Escreva um programa em Java, que leia uma string, gere uma nova string com o texto invertido e imprima esta nova string
+
+package br.com.eduardo.logic.strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex77InversaoDeString {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite uma palavra: ");
+            String word = scanner.next();
+            
+            char[] vetor = new char[word.length()];
+
+            int j = 0;
+            for (int i = vetor.length - 1; i >= 0; i--) {
+                char letter = word.charAt(i);
+                vetor[j] = letter;
+                j++;
+            }
+
+            System.out.println(new String(vetor));
+        }
+    }
+}
