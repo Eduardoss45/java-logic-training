a2b4a1132d0e29178bad13b5fa97e993c6bcbf9d
Eduardoss45 <temporariac432@gmail.com>
2026-08-19 20:23:13 -0300

    feat: Exercício 89.

---
+++ src/main/java/br/com/eduardo/logic/strings/Ex89UlimoNomeSeguidoDeIniciais.java
@@ -0,0 +1,29 @@
+package br.com.eduardo.logic.strings;
+
+import java.util.Scanner;
+
+// * 89. Escreva um programa em Java, que gere uma string composta pelo último nome, seguido de virgula e as iniciais dos demais nomes (em ordem), seguida de ponto. Por exemplo, se a string entrada for “Gabriel Garcia Marquez”, a string gerada deve ser “Marquez,G.G.”.
+
+public class Ex89UlimoNomeSeguidoDeIniciais {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite seu nome completo: ");
+            String text = scanner.nextLine();
+            String[] name = text.split(" ");
+
+            String abbreviatedName = "";
+
+            abbreviatedName += name[name.length - 1] + ",";
+
+            for (int i = 0; i < name.length - 1; i++) {
+                if (i < name.length - 2) {
+                    abbreviatedName += name[i].charAt(0) + ".";
+                } else {
+                    abbreviatedName += name[i].charAt(0);
+                }
+            }
+
+            System.out.printf("O nome \"%s\" abreviado ficou \"%s\"", text, abbreviatedName);
+        }
+    }
+}
